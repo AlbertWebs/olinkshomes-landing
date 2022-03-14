07849d97eae7ff74ab9e5ddc60272adeaa03f818
AlbertWebs <albertmuhatia@gmail.com>
2022-03-14 08:22:58 +0300

    admins panel

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,8 +16,14 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('image');
+            $table->string('mobile');
+            $table->string('address');
+            $table->string('country');
+            $table->integer('status');
+            $table->string('email');
             $table->timestamp('email_verified_at')->nullable();
+            $table->boolean('is_admin')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+
+Route::get('admin/home', [AdminsController::class, 'index'])->name('admin.home')->middleware('is_admin');
