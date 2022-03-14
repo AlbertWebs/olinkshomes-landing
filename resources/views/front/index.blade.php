4213aa2ee40dfce7a2756a2859a1bf75ec717a44
AlbertWebs <albertmuhatia@gmail.com>
2022-03-14 11:18:21 +0300

    TawkTo

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -29,6 +29,19 @@
         },
       });
     </script>
+    <!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+        (function(){
+        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+        s1.async=true;
+        s1.src='https://embed.tawk.to/622ef9d61ffac05b1d7e70ec/1fu3nvrii';
+        s1.charset='UTF-8';
+        s1.setAttribute('crossorigin','*');
+        s0.parentNode.insertBefore(s1,s0);
+        })();
+    </script>
+    <!--End of Tawk.to Script-->
     <!-- [if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js" type="text/javascript"></script><![endif] -->
     <script type="text/javascript">
       !(function (o, c) {
