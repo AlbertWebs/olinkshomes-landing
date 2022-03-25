174b3d7ff91ab0e211b3ebfbf92dfe432a24c9bc
AlbertWebs <albertmuhatia@gmail.com>
2022-03-25 09:57:52 +0300

    update tawk to

--- resources/views/front/masters-services.blade.php
+++ resources/views/front/masters-services.blade.php
@@ -20,6 +20,21 @@
     <link rel="stylesheet" href="{{asset('olinkshomes/css/custom.css')}}" type="text/css" />
     <link rel="stylesheet" href="{{asset('olinkshomes/css/colors6bbb.css?color=F18052')}}" type="text/css" />
     <link rel="stylesheet" href="{{asset('olinkshomes/css/custom.css')}}" type="text/css" />
+    {{--  --}}
+    <!--Start of Tawk.to Script-->
+   <script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/622ef9d61ffac05b1d7e70ec/1fu3nvrii';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
+    </script>
+    <!--End of Tawk.to Script-->
+     {{--  --}}
    </head>
    <body class="stretched">
       <div id="wrapper" class="clearfix">
